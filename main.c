afb2e6936bdf63de91752de6e81726c31398a91c
Christian Persch <chpe@src.gnome.org>
2009-01-06 18:19:39 +0000

    Move setlocale call and gettext and games-debug initialisation into games_runtime_init(), and adapt callers accordingly.
    svn path=/trunk/; revision=8470

--- main.c
+++ main.c
@@ -1044,21 +1044,10 @@ main (int argc, char **argv)
   gboolean retval;
   GError *error = NULL;
 
-#if defined(HAVE_GNOME) || defined(HAVE_RSVG_GNOMEVFS)
-  /* If we're going to use gnome-vfs, we need to init threads before
-   * calling any glib functions.
-   */
-  g_thread_init (NULL);
-#endif
-  
   if (!games_runtime_init ("gnibbles"))
     return 1;
 
   setgid_io_init ();
-
-  bindtextdomain (GETTEXT_PACKAGE, games_runtime_get_directory (GAMES_RUNTIME_LOCALE_DIRECTORY));
-  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-  textdomain (GETTEXT_PACKAGE);
 
   context = g_option_context_new (NULL);
 
