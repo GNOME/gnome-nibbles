1fc929f17dd9fd0fa625ff854057a64eff11c7c9
Michael Catanzaro <mcatanzaro@gnome.org>
2015-06-27 10:50:02 -0500

    Modernize autogen.sh
    Goodbye gnome-common

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from GNOME git (or from"
-    echo "your OS vendor's package manager)."
-    exit 1
+(test -f $srcdir/configure.ac) || {
+        echo "**Error**: Directory "\`$srcdir\'" does not look like the top-level project directory"
+        exit 1
 }
 
-REQUIRED_AUTOMAKE_VERSION=1.9
-REQUIRED_GETTEXT_VERSION=0.12
+PKG_NAME=`autoconf --trace 'AC_INIT:$1' "$srcdir/configure.ac"`
 
-. gnome-autogen.sh
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+        echo "**Warning**: I am going to run \`configure' with no arguments." >&2
+        echo "If you wish to pass any to it, please specify them on the" >&2
+        echo \`$0\'" command line." >&2
+        echo "" >&2
+fi
+
+set -x
+autoreconf --verbose --force --install -Wno-portability || exit 1
+{ set +x; } 2>/dev/null
+
+if [ "$NOCONFIGURE" = "" ]; then
+        set -x
+        $srcdir/configure "$@" || exit 1
+        { set +x; } 2>/dev/null
+
+        if [ "$1" = "--help" ]; then exit 0 else
+                echo "Now type \`make\' to compile $PKG_NAME" || exit 1
+        fi
+else
+        echo "Skipping configure process."
+fi
